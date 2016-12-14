a73bf14d72c77e4fe291bdb25fe0b5a0957fde5e
oronanschel <oronanschel@gmail.com>
2016-12-14 17:13:53 +0200

    clean version

--- main.py
+++ main.py
@@ -8,63 +8,50 @@ import tensorflow as tf
 import time
 from matplotlib.backends.backend_pdf import PdfPages
 import os
+
+
 class Trainer(object):
-    def __init__(self,in_test=True,in_train=True,samp_size=256,filename=None,num_of_prime=10,plot=False,
-                 plot_interval=3,alpha=1,sgd_batch_size=32,hid_size=80,lr=0.01,random_theta=False):
-      self.v_str_time_limit = 150
+    def __init__(self,hid_size=80,sgd_batch_size=32,filename=None,plot_on_screen=False):
+
+      ###### MDP Params ######
       self.bins = 40
-      self.batch_size = samp_size
-      self.num_of_prime = num_of_prime
-      self.plot_interval = plot_interval
-      self.gamma = 0.8
-      self.screen = np.zeros(shape=[self.bins,self.bins])
-      self.dist_l = []
+      self.gamma = 0.95
 
-      self.dist_l_p = [[] for _ in range(3)]
-      self.itr_l = []
-      self.filename= filename
-      self.plot = plot
-      self.alpha = alpha
-      self.sgd_batch_size = sgd_batch_size
-      self.save_itr =0
-
-
-      self.sess = tf.Session()
-      model = approximator.NN(self.sess,batch_size,num_of_prime)
-      self.model = approximator.NN_1_hot(self.sess,self.batch_size,self.num_of_prime,self.bins,in_train=in_train,
-                                        in_test=in_test,sgd_batch_size=self.sgd_batch_size,hid_size=hid_size,lr=lr,random_theta=random_theta)
-
-      # self.model = approximator.Tabular(bins=self.bins)
-
-
+      ###### Compute optimal V (baseline) ######
       self.Vstr =self.computeVstr()
 
+      ###### Initalzie a tf session with Value model ######
+      self.sess = tf.Session()
+      self.model = approximator.NN_1_hot(self.sess,self.bins,
+                                         sgd_batch_size=sgd_batch_size, hid_size=hid_size)
 
-      init_op = tf.initialize_all_variables()
-      self.sess.run(init_op)
+      # self.model = approximator.Tabular(bins=self.bins)
+      self.sess.run(tf.initialize_all_variables())
 
-
+      ###### Results/Plot ######
+      self.plot_interval = 1
+      self.filename = filename
       if filename:
           self.createResultFile()
+      self.plot_on_screen = plot_on_screen
+      if plot_on_screen:
+          plt.ion()
+          self.fig = plt.figure()
+          self.ax0 = self.fig.add_subplot(121, projection='3d')
+          self.ax1 = self.fig.add_subplot(122)
 
-      if plot:
-          # self.fig = plt.figure()
-          # self.ax0 = self.fig.add_subplot(331, projection='3d')
-          # self.ax1 = self.fig.add_subplot(332)
-          # self.ax2 = self.fig.add_subplot(333)
-          # self.ax3 = self.fig.add_subplot(334)
-          # self.ax4 = self.fig.add_subplot(335)
+      ###### Miscellaneous ######
+      self.epoch = 0
+      self.dist_l = []
+      self.epoch_l = []
 
-          self.f_tmp = plt.figure()
-          self.ax_tmp = self.f_tmp.add_subplot(111, projection='3d')
-          # plt.ion()
+
 
     def computeVstr(self):
         V_str = np.zeros(shape=[self.bins,self.bins])
         V_str_new = np.ones(shape=[self.bins,self.bins])
 
-        start_time = time.time()
-        iternum = 10000
+        iternum = 1000
         for itr in range(iternum):
             V_str = np.array(V_str_new)
             for idx in range(self.bins):
@@ -75,19 +62,15 @@ class Trainer(object):
                              , V_str[idx                   ,min(idy + 1, self.bins - 1)]]
                     V_max = np.max(V_list)
 
-                    V_str_new[idx,idy] = self.gamma*V_max + self.computeR_idx(idx,idy)
-
-            time_now = time.time()
-            if  time_now - start_time > self.v_str_time_limit:
-                print 'time limit reached'
-                break
-            if np.mean(np.square(V_str.reshape(-1)-V_str_new.reshape(-1)))<10**-12:
+                    V_str_new[idx,idy] = self.gamma*V_max + self.reward(idx,idy)
+            if np.mean(np.square(V_str.reshape(-1)-V_str_new.reshape(-1)))<10**-13:
                 break
 
-        print 'Vstr eps='+str(np.mean(np.square(V_str.reshape(-1)-V_str_new.reshape(-1))))
+        print 'Optimal V with eps='+str(np.mean(np.square(V_str.reshape(-1)-V_str_new.reshape(-1)))) \
+              + ' in '+str(itr)+' iterations'
         return V_str_new
 
-    def computeR_idx(self,idx,idy):
+    def reward(self,idx,idy):
         r = 0
         if idx==39 and idy==39:
             r = 1
@@ -96,7 +79,6 @@ class Trainer(object):
         return r
 
     def plotVstr(self):
-
         x = np.repeat(np.arange(self.bins),self.bins)
         y = np.arange(self.bins)
         for _ in range(1,self.bins):
@@ -118,102 +100,96 @@ class Trainer(object):
         pp.savefig(fig)
         pp.close()
 
-        # plot problem example path
-        fig = plt.figure()
-        ax = fig.gca()
-
-        x = [1,1,1]
-        y = [2,3,4]
-        ax.plot(x, y,color='b',marker=6)
-
-        x = [1,2]
-        y = [4,4]
-        ax.plot(x, y,color='b')
-
-        x = [2,3,4,5,6,7,8]
-        y = [4,4,4,4,4,4,4]
-        ax.plot(x, y,color='b', marker='>')
-
-        x = [8,8]
-        y = [4,5]
-        ax.plot(x, y,color='b')
-
-        x = [8,8,8,8,8,8]
-        y = [5,6,7,8,9,10]
-        ax.plot(x, y, color='b', marker=6)
-
-        x = [8,7]
-        y = [10,10]
-        ax.plot(x, y,color='b')
-
-
-        x = [7,6,5]
-        y = [10,10,10]
-        ax.plot(x, y,color='b', marker='<')
-
-        ax.plot([5],[5], '*r',markersize=18)
-        ax.text(5.2, 5.2, '$r=1$', fontsize=22)
-
-        ax.set_xlim(0,10.5)
-        ax.set_ylim(0,10.5)
-
-        plt.xticks([0,2.5,5,7.5,10],[0,10,20,30,40])
-        plt.yticks([0,2.5,5,7.5,10],[0,10,20,30,40])
-
-        plt.title('Gridworld')
-        plt.xlabel('x')
-        plt.ylabel('y')
-
-        pp = PdfPages("Gridworld.pdf")
-        pp.savefig(fig)
-        pp.close()
-
-
+        # # plot problem example path
+        # fig = plt.figure()
+        # ax = fig.gca()
+        #
+        # x = [1,1,1]
+        # y = [2,3,4]
+        # ax.plot(x, y,color='b',marker=6)
+        #
+        # x = [1,2]
+        # y = [4,4]
+        # ax.plot(x, y,color='b')
+        #
+        # x = [2,3,4,5,6,7,8]
+        # y = [4,4,4,4,4,4,4]
+        # ax.plot(x, y,color='b', marker='>')
+        #
+        # x = [8,8]
+        # y = [4,5]
+        # ax.plot(x, y,color='b')
+        #
+        # x = [8,8,8,8,8,8]
+        # y = [5,6,7,8,9,10]
+        # ax.plot(x, y, color='b', marker=6)
+        #
+        # x = [8,7]
+        # y = [10,10]
+        # ax.plot(x, y,color='b')
+        #
+        #
+        # x = [7,6,5]
+        # y = [10,10,10]
+        # ax.plot(x, y,color='b', marker='<')
+        #
+        # ax.plot([5],[5], '*r',markersize=18)
+        # ax.text(5.2, 5.2, '$r=1$', fontsize=22)
+        #
+        # ax.set_xlim(0,10.5)
+        # ax.set_ylim(0,10.5)
+        #
+        # plt.xticks([0,2.5,5,7.5,10],[0,10,20,30,40])
+        # plt.yticks([0,2.5,5,7.5,10],[0,10,20,30,40])
+        #
+        # plt.title('Gridworld')
+        # plt.xlabel('x')
+        # plt.ylabel('y')
+        #
+        # pp = PdfPages("Gridworld.pdf")
+        # pp.savefig(fig)
+        # pp.close()
         return
 
-    def sample_and_target(self,batch_size):
-        s = np.zeros(shape=[batch_size,self.bins,self.bins])
-        V_t = np.zeros(batch_size)
-
-        # TODO: "confuse" with histoy of state
-        for i in range(batch_size):
-            idx = np.random.randint(self.bins)
-            idy = np.random.randint(self.bins)
-            s[i,idx,idy] = 1
-            r = self.computeR_idx(idx,idy)
-            s1 = np.zeros(shape=[4,self.bins,self.bins])
-            s1[0,min(idx + 1, self.bins - 1), idy] = 1
-            s1[1,max(idx - 1, 0), idy] = 1
-            s1[2,idx,max(idy - 1, 0)] =1
-            s1[3,idx,min(idy + 1, self.bins - 1)]=1
-            alpha = self.computeAlpha()
-            V_t[i] = r+self.gamma*np.max(self.model.predict(s1,testing=False,alpha=alpha))
-
-        return s,V_t
+    # def sample_and_target(self,batch_size):
+    #     s = np.zeros(shape=[batch_size,self.bins,self.bins])
+    #     V_t = np.zeros(batch_size)
+    #
+    #     for i in range(batch_size):
+    #         idx = np.random.randint(self.bins)
+    #         idy = np.random.randint(self.bins)
+    #         s[i,idx,idy] = 1
+    #         r = self.reward(idx,idy)
+    #         s1 = np.zeros(shape=[4,self.bins,self.bins])
+    #         s1[0,min(idx + 1, self.bins - 1), idy] = 1
+    #         s1[1,max(idx - 1, 0), idy] = 1
+    #         s1[2,idx,max(idy - 1, 0)] =1
+    #         s1[3,idx,min(idy + 1, self.bins - 1)]=1
+    #         alpha = self.computeAlpha()
+    #         V_t[i] = r+self.gamma*np.max(self.model.predict(s1))
+    #
+    #     return s,V_t
 
     def full_ss_and_target(self):
         s = np.zeros(shape=[self.bins**2,self.bins,self.bins])
         V_t = np.zeros(self.bins**2)
 
-        # TODO: "confuse" with histoy of state
         i=0
         for idx in range(self.bins):
             for idy in range(self.bins):
                 s[i,idx,idy] = 1
-                r = self.computeR_idx(idx,idy)
+                r = self.reward(idx,idy)
                 s1 = np.zeros(shape=[4,self.bins,self.bins])
                 s1[0,min(idx + 1, self.bins - 1), idy] = 1
                 s1[1,max(idx - 1, 0), idy] = 1
                 s1[2,idx,max(idy - 1, 0)] =1
                 s1[3,idx,min(idy + 1, self.bins - 1)]=1
-                alpha = self.computeAlpha()
-                V_t[i] = r+self.gamma*np.max(self.model.predict(s1,testing=False,alpha=alpha))
+                V_t[i] = r+self.gamma*np.max(self.model.predict(s1))
                 i+=1
 
         return s,V_t
 
     def createResultFile(self):
-
         self.res_file = os.path.join(os.getcwd(), self.filename+'.csv')
         res_file = open(self.res_file, 'wb')
         res_file.write('itr,dist\n')
@@ -229,24 +205,15 @@ class Trainer(object):
                 V_str_flat[itr]=self.Vstr[idx,idy]
                 itr+=1
 
-        V_pred = self.model.predict(s,testing=True,alpha=self.alpha)
-        V_diff = V_pred.reshape(itr)-V_str_flat.reshape(itr)
+        V_pred = self.model.predict(s)
         dist = np.mean(np.square(V_pred.reshape(itr)-V_str_flat.reshape(itr)))
 
-        # self.dist_l_p[0].append(np.square(V_pred[5]-V_str_flat[5]))
-        # self.dist_l_p[1].append(np.square(V_pred[44]-V_str_flat[44]))
-        # self.dist_l_p[2].append(np.square(V_pred[102]-V_str_flat[102]))
-
-
-        self.dist_l_p[0].append((V_pred[5]))
-        self.dist_l_p[1].append((V_pred[44]))
-        self.dist_l_p[2].append((V_pred[102]))
 
         self.dist_l.append(dist)
-        self.itr_l.append(self.itr)
+        self.epoch_l.append(self.epoch)
         if self.filename:
             res_file = open(self.res_file, 'a')
-            res_file.write("%d,%.11f\n" % (self.itr,dist))
+            res_file.write("%d,%.11f\n" % (self.epoch,dist))
             res_file.close()
 
         x = np.repeat(np.arange(self.bins),self.bins)
@@ -254,46 +221,28 @@ class Trainer(object):
         for _ in range(1,self.bins):
             y = np.append(y,np.arange(self.bins))
 
-        if self.plot and self.itr % 5 == 0:
-            self.ax_tmp.clear()
-            self.ax_tmp.plot_trisurf(x, y, V_pred.reshape(len(x)), cmap=cm.jet, linewidth=0.2,vmin=0,vmax=5)
-            # self.ax_tmp.axis(0,40,0,40,0,5)
-            # self.f_tmp.set_size_inches(5, 5)
-            self.f_tmp.savefig('q_learning'+str(self.save_itr), dpi=50)
-            self.save_itr+=1
+        if self.plot_on_screen and self.epoch % 1 == 0:
+            self.ax0.clear()
+            self.ax1.clear()
+            self.ax0.plot_trisurf(x, y, V_pred.reshape(len(x)), cmap=cm.jet, linewidth=0.2,vmin=0,vmax=5)
+            self.ax1.plot(self.epoch_l,self.dist_l)
+            self.ax1.set_yscale('log')
+            plt.pause(0.05)
 
-            # self.ax0.clear()
-            # self.ax1.clear()
-            #
-            # self.ax2.clear()
-            # self.ax3.clear()
-            # self.ax4.clear()
 
-            # self.ax0.plot_trisurf(x, y, V_pred.reshape(len(x)), cmap=cm.jet, linewidth=0.2)
-            # self.ax0.plot_trisurf(x, y, V_diff.reshape(len(x)), cmap=cm.jet, linewidth=0.2)
-            # self.ax1.semilogy(self.itr_l,self.dist_l)
 
-            # self.ax2.semilogy(self.itr_l,self.dist_l_p[0])
-            # self.
 
-            # plt.axis((self.itr-300,self.itr, 10**-12, 10**-1))
-            # self.ax3.semilogy(self.itr_l, self.dist_l_p[1])
-            # plt.axis((self.itr - 300, self.itr, 10 ** -12, 10 ** -1))
-            # self.ax4.semilogy(self.itr_l, self.dist_l_p[2])
-            # plt.axis((self.itr - 300, self.itr, 10 ** -12, 10 ** -1))
 
-            # plt.pause(0.05)
 
         return dist
 
-    def Train(self,iternum,sgd_itrenum,full_state_update=True):
+    def Train(self,epoch_num,sgd_itrenum,full_state_update=True):
 
-        for self.itr in range(iternum):
-            if self.itr % self.plot_interval == 0:
+        for self.epoch in range(epoch_num):
+            if self.epoch % self.plot_interval == 0:
                     self.computeDist()
-            if self.itr-1%5==0:
-                #plt.pause(0.01)
-                print '------ itr:'+str(self.itr)+ '------'
+            if self.epoch-1%5==0:
+                print '------ epoch:'+str(self.epoch)+ '------'
 
             if full_state_update:
                 s,V_t = self.full_ss_and_target()
@@ -309,39 +258,22 @@ class Trainer(object):
         # return self.alpha*(10)/(10+self.itr)
 
 
-iternum = 700
+
+epoch_num = 700
 sgd_itrenum = 100
 samp_size = 300
-primes = 1
-full_state_update = False
-alpha = 0.001
-# alpha = 1 # TODO: (decline in time)
-batch_size = 32
+full_state_update = True
+sgd_batch_size = 32
 hid_size = 80
-random_theta = False
+plot_on_screen = True
 
-lr = 0.8
+filename = 'results/full_s_sgd_'+str(sgd_itrenum)+'_sgdbatchsize_'+str(sgd_batch_size)\
+       +'_hidsize_'+str(hid_size)
 
-# name = 'results/Bfull_s_sgd_'+str(sgd_itrenum)+'_prime_'+str(primes)+'_alpha_'+str(alpha)+'_batch_size_'+str(batch_size)+'_hidsize_'+str(hid_size)
+print filename
 
-
-name = 'results/full_s_sgd_'+str(sgd_itrenum)+'_prime_'+str(primes)+'_alpha_'+str(alpha)+'_batch_size_'+str(batch_size)\
-       +'_hidsize_'+str(hid_size)+'_lr_'+str(lr)+'_random_theta_'+str(random_theta)
-
-
-# name = 'results/samp_size_'+str(samp_size)+'_sgd_'+str(sgd_itrenum)+'_prime_'+str(primes)+'_alpha_'+str(alpha)+'_batch_size_'+str(batch_size)+'_hidsize_'+str(hid_size)
-
-# name = None
-# plot_interval  =1
-# plot=True
-
-print name
-# train = Trainer(samp_size,filename=name,num_of_prime=primes,plot_interval=plot_interval,plot=plot)
-train = Trainer(filename=name,num_of_prime=primes,in_test=True,in_train=True,alpha=alpha,
-                sgd_batch_size=batch_size,hid_size=hid_size,lr=lr,random_theta=random_theta,plot=True)
+train = Trainer(hid_size=hid_size,sgd_batch_size=sgd_batch_size,filename=filename,plot_on_screen=plot_on_screen)
+# train.plotVstr()
+train.Train(epoch_num,sgd_itrenum,full_state_update=full_state_update)
 
 
-train.Train(iternum,sgd_itrenum,full_state_update=full_state_update)
-
-
-train.plotVstr()
--- model.py
+++ model.py
@@ -6,19 +6,16 @@ from ops import linear
 
 class Tabular(object):
     def __init__(self, bins):
-
         self.bins = bins
         self.V = np.random.randn(bins,bins)
 
-
-    def learn(self, s, target, N):
+    def learn(self, s, target):
         for i in range(len(s)):
             idx = np.unravel_index(s[i].argmax(),s[0].shape)
+            ###### alpha = 0.5 ######
             self.V[idx] = 0.5*self.V[idx] + 0.5*target[i]
 
-        return 1
-
-    def predict(self, s,testing,alpha):
+    def predict(self, s):
         pred = np.zeros(len(s))
         for i in range(len(s)):
             idx = np.unravel_index(s[i].argmax(),s[0].shape)
@@ -27,78 +24,37 @@ class Tabular(object):
         return pred
 
 
-
-
 class NN_1_hot(object):
-    def __init__(self, sess, batch_size,num_of_prime,bins,in_train,in_test,sgd_batch_size,hid_size,lr,random_theta):
+    def __init__(self, sess,bins,sgd_batch_size,hid_size):
         self.sess = sess
-        self.batch_size = batch_size
-        self.mini_batch_size =sgd_batch_size
-        self.num_of_prime = num_of_prime
+        self.mini_batch_size = sgd_batch_size
         self.bins = bins
-        self.in_train = in_train
-        self.in_test = in_test
         self.hid_size=hid_size
-        self.lr=lr
-        self.t =0
-        self.random_theta = random_theta
-        self.g_noise = None
+
 
         self.build_net()
 
-    def learn(self, s, target, N):
+    def learn(self, s, target, iternum):
 
         target = target.reshape(target.shape[0],1)
-        print_rnd = np.random.uniform()
-        # print_rnd = 0
-        if self.random_theta:
-        # Assaing random values for learning network
-            for name in self.w.keys():
-             w = self.sess.run([tf.random_normal(shape=self.w[name].get_shape().as_list(),stddev=0.02)])
-             self.sess.run([self.w_assign_op[name]], {self.t_w_input[name]: w[0]})
-        if print_rnd<0.05:
-          print 'loss'
-        for itr in range(N):
+
+        for itr in range(iternum):
             idx = np.random.randint(low=0,high=target.shape[0],size=self.mini_batch_size)
             mini_batch = target[idx,...]
             s_t = s[idx,...]
-            comp_lr = self.lr*(N)/(N+3*itr)
-            _, loss = self.sess.run([self.optim, self.loss], {self.s_t: s_t, self.target: mini_batch,self.learning_rate:comp_lr})
-
-            if itr % 400 == 0:
-                if print_rnd<0.05:
+            _, loss = self.sess.run([self.optim, self.loss], {self.s_t: s_t, self.target: mini_batch})
+            if itr % 50 == 0:
                     print 'itr:' + str(itr) + ' loss:' + str(loss)
-        if print_rnd<0.05:
-            print 'itr:' + str(itr) + ' loss:' + str(loss)
-
-        self.update_target_q_network()
-        return
-
-    def predict(self, s,testing,alpha):
-        # pred = self.sess.run([self.pred], {self.s_t: s})[0]
-        # sum = 1
-        # for k in range(1,self.num_of_prime):
-        #     pred += self.sess.run([self.target_list[k-1]], {self.s_t: s})[0] *(1-alpha)**(k)
-        #     sum  += (1-alpha)**(k)
-        # pred = pred / (sum)
-        # #
-
-        pred = self.sess.run([self.pred], {self.s_t: s})[0] * alpha
-        sum = alpha
-        for k in range(1, self.num_of_prime):
-            pred += self.sess.run([self.target_list[k-1]], {self.s_t: s})[0] * (1 - alpha)
-            sum+=(1-alpha)
-        pred = pred/sum
+        print 'itr:' + str(itr) + ' loss:' + str(loss)
 
 
-
+    def predict(self, s):
+        pred = self.sess.run([self.pred], {self.s_t: s})[0]
         return pred
 
 
     def build_net(self):
         self.w = {}
-        self.t_w = []
-        self.target_list =[[] for k in range(self.num_of_prime)]
 
         hidden_size = self.hid_size
         self.s_t = tf.placeholder('float32', [None, self.bins, self.bins], name='s_t')
@@ -110,60 +66,9 @@ class NN_1_hot(object):
             hid, self.w['l0_w'], self.w['l0_b'] = linear(self.s_t_flat, hidden_size, activation_fn=tf.nn.relu, name='l0')
             self.pred ,self.w['l1_w'], self.w['l1_b'] = linear(hid, 1, activation_fn=None, name='l1')
 
-        for k in range(self.num_of_prime):
-            self.t_w.append({})
-            with tf.variable_scope('target'+str(k)):
-                hid, self.t_w[k]['l0_w'], self.t_w[k]['l0_b'] = linear(self.s_t_flat, hidden_size, activation_fn=tf.nn.relu,
-                                                             name='l0')
-                self.target_list[k], self.t_w[k]['l1_w'], self.t_w[k]['l1_b'] = linear(hid, 1, activation_fn=None, name='l1')
-
-        with tf.variable_scope('pred_to_target'):
-            self.t_w_input = {}
-            self.t_w_assign_op = {}
-
-
-            self.w_assign_op = {}
-
-            for name in self.w.keys():
-                self.t_w_input[name] = tf.placeholder('float32', self.t_w[0][name].get_shape().as_list(), name=name)
-                self.t_w_assign_op[name] = self.t_w[0][name].assign(self.t_w_input[name])
-                self.w_assign_op[name] = self.w[name].assign(self.t_w_input[name])
-
-            self.t_w_input_primes = []
-            self.t_w_assign_op_primes = []
-            for k in range(0,self.num_of_prime-1):
-                self.t_w_assign_op_primes.append({})
-                for name in self.w.keys():
-                    self.t_w_assign_op_primes[k][name] = self.t_w[k+1][name].assign(self.t_w_input[name])
-
-
 
         with tf.variable_scope('optimizer'):
 
             self.delta = tf.reshape(self.pred, shape=[self.mini_batch_size]) - tf.reshape(self.target,shape=[self.mini_batch_size])
             self.loss = tf.reduce_mean(tf.square(self.delta))
-
-            self.learning_rate = tf.placeholder('float32', name='learning_rate')
-
-            # self.optim = tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate ).minimize(self.loss)
-            # self.optim = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate, decay=0.9, momentum=0.0, epsilon=1e-10).minimize(self.loss)
             self.optim = tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
-
-    def update_target_q_network(self):
-
-        for k in range(self.num_of_prime-2,-1,-1):
-            for name in self.w.keys():
-                w = self.sess.run(self.t_w[k][name])
-                self.sess.run([self.t_w_assign_op_primes[k][name]],{self.t_w_input[name]: w})
-
-        for name in self.w.keys():
-            w = self.sess.run(self.w[name])
-            self.sess.run([self.t_w_assign_op[name]], {self.t_w_input[name]: w})
-
-    def random_init_w(self):
-        # Assaing random values for learning network
-            for name in self.w.keys():
-             w = self.sess.run([tf.random_normal(shape=self.w[name].get_shape().as_list(),stddev=0.02)])
-             self.sess.run([self.w_assign_op[name]], {self.t_w_input[name]: w[0]})
-
-
